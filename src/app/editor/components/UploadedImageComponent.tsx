8e71dbdc04342008fac5b15b9e52d6de71f43740
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-14 00:10:25 +0900

    refactor: add custom file select label

--- src/app/editor/components/UploadedImageComponent.tsx
+++ src/app/editor/components/UploadedImageComponent.tsx
@@ -19,11 +19,18 @@ export default function UploadedImageComponent({canvasRef, setImage}: UploadedIm
     <div className="relative flex flex-1 border-2 h-full rounded-lg p-4 flex-col items-center justify-center text-center">
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
         <h2 className="font-bold text-2xl m-4">이미지를 업로드하여 편집을 시작하세요</h2>
+        <label
+          htmlFor="fileUpload"
+          className="px-6 py-3 bg-white/10 text-white rounded-lg cursor-pointer text-center"
+        >
+          파일 선택
+        </label>
         <input
+          id="fileUpload"
           type="file"
           accept="image/*"
           onChange={handleImageUpload}
-          className="bg-white/10 text-white px-4 py-2 rounded border border-white/20"
+          className="hidden"
         />
       </div>
 
