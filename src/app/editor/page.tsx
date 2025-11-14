d09fbe7992af404abc0a21c81cd8368d3cdd33ee
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-14 17:05:31 +0900

    refactor: split wasm loading function and initialize canvas

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,35 +1,17 @@
 "use client"
 
-import {useEffect, useRef, useState} from "react";
-import {WasmModule} from "@/lib/wasm-loader";
 import UploadedImageComponent from "@/app/editor/components/ImageFile/UploadedImageComponent";
 import GrayScaleComponent from "@/app/editor/components/GrayScaleComponent";
-import useCanvasUtils from "@/app/hooks/useCanvasUtils";
+import useImageEditor from "@/app/hooks/useImageEditor";
 
 export default function EditorPage() {
-  const [wasm, setWasm] = useState<WasmModule | null>(null);
-  const [image, setImage] = useState<HTMLImageElement | null>(null);
-  const [originalPixels, setOriginalPixels] = useState<ImageData["data"]  | null>(null);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const { initializeCanvas } = useCanvasUtils({ canvasRef });
-
-  useEffect(() => {
-    const initWasm = async () => {
-      const { loadWASM } = await import('@/lib/wasm-loader');
-      const wasmModule = await loadWASM();
-      setWasm(wasmModule);
-    };
-    initWasm();
-  }, []);
-
-  useEffect(() => {
-    if (!image) return;
-
-    const info = initializeCanvas(image);
-    if (!info) return;
-
-    setOriginalPixels(new Uint8ClampedArray(info.data));
-  }, [image]);
+  const {
+    wasm,
+    image,
+    setImage,
+    originalPixels,
+    canvasRef
+  } = useImageEditor();
 
   return (
     <div className="flex flex-row bg-black text-white h-screen gap-6 p-12">
--- src/app/hooks/useImageEditor.ts
+++ src/app/hooks/useImageEditor.ts
@@ -1,3 +1,37 @@
-export default function useImageEditor() {
+import {useEffect, useRef, useState} from "react";
+import {WasmModule} from "@/lib/wasm-loader";
+import useCanvasUtils from "@/app/hooks/useCanvasUtils";
 
+export default function useImageEditor() {
+  const [wasm, setWasm] = useState<WasmModule | null>(null);
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const [originalPixels, setOriginalPixels] = useState<ImageData["data"]  | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const { initializeCanvas } = useCanvasUtils({ canvasRef });
+
+  useEffect(() => {
+    const initWasm = async () => {
+      const { loadWASM } = await import('@/lib/wasm-loader');
+      const wasmModule = await loadWASM();
+      setWasm(wasmModule);
+    };
+    initWasm();
+  }, []);
+
+  useEffect(() => {
+    if (!image) return;
+
+    const info = initializeCanvas(image);
+    if (!info) return;
+
+    setOriginalPixels(new Uint8ClampedArray(info.data));
+  }, [image]);
+
+  return {
+    wasm,
+    image,
+    setImage,
+    originalPixels,
+    canvasRef
+  };
 }
