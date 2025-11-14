35244450c32f8ed7c96109b835a656403b093d73
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-14 17:29:41 +0900

    chore: delete direct webpack setting

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  }
+  turbopack: {},
 };
 
 export default nextConfig;
