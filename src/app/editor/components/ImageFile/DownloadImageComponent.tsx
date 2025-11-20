b94a2abaaff4f174de316e683db5327d7427cb58
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-20 19:12:26 +0900

    refactor: check web brower is ios safari

--- src/app/editor/components/ImageFile/DownloadImageComponent.tsx
+++ src/app/editor/components/ImageFile/DownloadImageComponent.tsx
@@ -3,19 +3,54 @@ interface DownloadImageComponentProps {
 }
 
 export default function DownloadImageComponent({ canvasRef }: DownloadImageComponentProps) {
-  const handleDownload  = () => {
+  const handleDownload  = async () => {
     if (!canvasRef.current) return;
 
-    canvasRef.current?.toBlob((blob) => {
-      if (!blob) return;
+    const isIOS =
+      /iPad|iPhone|iPod/.test(navigator.userAgent);
 
+    const blob =
+      await new Promise<Blob | null>((resolve) => {
+        if (canvasRef.current?.toBlob) {
+          canvasRef.current?.toBlob((b) => resolve(b), "image/png");
+        } else {
+          const dataURL = canvasRef.current?.toDataURL("image/png");
+          resolve(dataURLToBlob(dataURL));
+        }
+      });
+
+    if (!blob) return;
+
+    if (isIOS) {
       const url = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = "edited-image.png";
-      link.click();
-      URL.revokeObjectURL(url);
-    }, "image/png");
+
+      const win = window.open(url, "_blank");
+      if (!win) alert("팝업 차단 해제 후 다시 시도하세요.");
+
+      return;
+    }
+
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "edited-image.png";
+    link.click();
+
+    URL.revokeObjectURL(url);
+  };
+
+  const dataURLToBlob = (dataURL: string): Blob => {
+    const parts = dataURL.split(",");
+    const mime = parts[0].match(/:(.*?);/)![1];
+    const binary = atob(parts[1]);
+    const len = binary.length;
+    const arr = new Uint8Array(len);
+
+    for (let i = 0; i < len; i++) {
+      arr[i] = binary.charCodeAt(i);
+    }
+
+    return new Blob([arr], { type: mime });
   };
 
   return (
