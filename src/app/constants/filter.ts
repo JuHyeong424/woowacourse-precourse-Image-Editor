bf87f67e44f71e55bc6aaaf8a190c524c5714a17
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-21 01:48:35 +0900

    refactor: extract constants

--- src/app/constants/filter.ts
+++ src/app/constants/filter.ts
@@ -1,4 +1,4 @@
-import { BooleanFilterKey } from "@/app/types/filterStateTypes";
+import {BooleanFilterKey, NumberFilterKey} from "@/app/types/filterStateTypes";
 
 export const BUTTON_FILTERS: {
   key: BooleanFilterKey;
@@ -9,4 +9,18 @@ export const BUTTON_FILTERS: {
   { key: "invert", label: "색 반전 필터", id: "invert" },
   { key: "blur", label: "블러 필터", id: "blur" },
   { key: "sharpen", label: "선명도 필터", id: "sharpen" },
+];
+
+export const SLIDER_FLITER: { key: NumberFilterKey; label: string; min: number; max: number; }[] = [
+  { key: "brightness", label: "밝기 조절", min: 0, max: 200 },
+  { key: "contrast", label: "대비 조절", min: 0, max: 200 },
+  { key: "saturation", label: "채도 조절", min: 0, max: 200 },
+  { key: "exposure", label: "감마 조절", min: -100, max: 100 },
+  { key: "hue", label: "색조 회전", min: 0, max: 360 },
+  { key: "temperature", label: "색온도", min: -100, max: 100 },
+  { key: "tint", label: "색감", min: -100, max: 100 },
+  { key: "shadows", label: "섀도우 조절", min: -100, max: 100 },
+  { key: "highlights", label: "하이라이트 조절", min: -100, max: 100 },
+  { key: "clarity", label: "로컬 대비 조절", min: -100, max: 100 },
+  { key: "vignette", label: "비네트 강도 조절", min: -100, max: 100 },
 ];
--- src/app/editor/components/FilterPanel/slider/SliderFilters.tsx
+++ src/app/editor/components/FilterPanel/slider/SliderFilters.tsx
@@ -1,25 +1,12 @@
 import SliderFilterComponent from "@/app/editor/components/FilterPanel/slider/SliderFilterComponent";
 import React from "react";
-import {FilterProps, NumberFilterKey} from "@/app/types/filterStateTypes";
+import {FilterProps} from "@/app/types/filterStateTypes";
+import {SLIDER_FLITER} from "@/app/constants/filter";
 
 export default function SliderFilters({ filters, setFilter, disabled }: FilterProps) {
-  const sliderFilters: { key: NumberFilterKey; label: string; min: number; max: number; }[] = [
-    { key: "brightness", label: "밝기 조절", min: 0, max: 200 },
-    { key: "contrast", label: "대비 조절", min: 0, max: 200 },
-    { key: "saturation", label: "채도 조절", min: 0, max: 200 },
-    { key: "exposure", label: "감마 조절", min: -100, max: 100 },
-    { key: "hue", label: "색조 회전", min: 0, max: 360 },
-    { key: "temperature", label: "색온도", min: -100, max: 100 },
-    { key: "tint", label: "색감", min: -100, max: 100 },
-    { key: "shadows", label: "섀도우 조절", min: -100, max: 100 },
-    { key: "highlights", label: "하이라이트 조절", min: -100, max: 100 },
-    { key: "clarity", label: "로컬 대비 조절", min: -100, max: 100 },
-    { key: "vignette", label: "비네트 강도 조절", min: -100, max: 100 },
-  ];
-
   return (
     <div className="flex flex-col gap-4">
-      {sliderFilters.map((filter) => (
+      {SLIDER_FLITER.map((filter) => (
         <SliderFilterComponent
           key={filter.key}
           disabled={disabled}
