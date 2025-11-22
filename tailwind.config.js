4a57dbce07b8cf52ed636f497f1e0f3b69c52b19
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-22 21:16:27 +0900

    chore: content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {},
