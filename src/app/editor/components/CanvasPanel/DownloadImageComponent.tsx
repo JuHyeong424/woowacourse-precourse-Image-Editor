e344f737df33f19fc5867a02d330878006d90506
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-11-24 10:06:45 +0900

    refactor: wrong name

--- src/app/editor/components/CanvasPanel/DownloadImageComponent.tsx
+++ src/app/editor/components/CanvasPanel/DownloadImageComponent.tsx
@@ -16,7 +16,7 @@ export default function DownloadImageComponent({ canvasRef }: DownloadImageCompo
     <button
       onClick={handleDownload}
       className="
-        small:text-xs meidum:text-xs tablet:text-sm laptop:text-sm
+        small:text-xs medium:text-xs tablet:text-sm laptop:text-sm
         absolute small:top-1 small:right-1 medium:top-1 medium:right-1 tablet:top-1 tablet:right-1 laptop:top-4 laptop:right-4 cursor-pointer z-20
         bg-white/70 hover:bg-white text-black small:px-2 medium:px-2 tablet:px-4 laptop:px-4 small:py-1 medium:py-1 tablet:py-2 laptop:py-2 rounded-md
         shadow font-medium transition m-4
